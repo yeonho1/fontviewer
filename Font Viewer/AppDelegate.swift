3b403db208c9c73738c0322b848f81748e9de108
Yeonho Lee <tomatoyeonho@naver.com>
2020-07-06 22:05:59 +0900

    Initial Commit

---
+++ Font Viewer/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Font Viewer
+//
+//  Created by 이연호 on 2020/07/06.
+//  Copyright © 2020 Yeonho Lee. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Font Viewer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Font Viewer
+//
+//  Created by 이연호 on 2020/07/06.
+//  Copyright © 2020 Yeonho Lee. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
